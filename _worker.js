ca936060cbc396b7438f1215b458f4064a8065eb
akani2984 <1320553934@qq.com>
2025-01-27 18:24:01 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -37,7 +37,7 @@ export default {
       }
       let blockfix = { method: request.method,
                      headers: request.headers,
-                     body:  request.body
+                     body:  request.arrayBuffer()
                      };
       let destinationurl = new URL(request.url)
       destinationurl.hostname = getCookie('hostname')
